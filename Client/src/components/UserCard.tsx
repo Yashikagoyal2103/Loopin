8e67c2500bed71424db1b8f0fe37d40db9590a60
Yashikagoyal2103 <yashikagoyal2005@gmail.com>
2025-08-22 23:34:41 +0530

    discover page added

---
+++ Client/src/components/UserCard.tsx
@@ -0,0 +1,56 @@
+import { dummyUserData } from '../assets/assets'
+import { type User } from '../assets/assets'
+import { MapPin, MessageCircle, Plus, UserPlus } from 'lucide-react'
+
+const UserCard = ({ user }: { user: User }) => {
+
+    const currentUser = dummyUserData
+
+    const handleFollow = async () => {
+
+    }
+
+    const handleConnectionRequest = async () => {   
+
+    }
+  return (
+    <div  key={user._id} className="p-4 pt-6 flex flex-col justify-between w-72 shadow border border-gray-200  rounded-md">
+        <div className='text-center'>
+            <img src={user.profile_picture} alt="Profile" className='rounded-full w-16 shadow-md mx-auto' />
+            <p className='mt-4 font-semibold'>{user.full_name}</p>
+            {user.username && <p className='font-light text-gray-500'>@{user.username}</p>}
+            {user.bio && <p className='text-gray-600 mt-2 px-4 text-center text-sm'>{user.bio}</p>}
+        </div>
+
+        <div className='flex items-center justify-center gap-2mt-4 text-xs text-gray-600 '>
+            <div className='flex items-center gap-1 border border-gray-300 rounded-full px-3 py-1'> 
+                <MapPin className='w-6 h-4'/>{user.location}
+            </div>
+            <div className='flex items-center gap-1 border border-gray-300 rounded-full px-3 py-1'>
+                <span>{user.followers.length}</span> Followers
+            </div>
+        </div>
+
+        <div className='flex mt-4 gap-2'>
+            {/* Follow Button */}
+            <button onClick={handleFollow} disabled={currentUser?.following.includes(user._id)} className='w-full py-2 rounded-md flex justify-center items-center gap-2 bg-gradient-to-r
+            from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active-scale-95 transition text-white cursor-pointer'>
+                <UserPlus />{currentUser?.following.includes(user._id) ? 'Following' : 'Follow' }
+            </button>
+
+            {/* Connection request button / message button */}
+            <button onClick={handleConnectionRequest} className='flex items-center justify-center w-16 border text-slate-500 group rounded-md cursor-pointer active:scalle-95 transition'>
+                {
+                    currentUser?.connections.includes(user._id) ? 
+                    <MessageCircle className="w-5 h-5 group-hover:scale-105 transition" />
+                    :
+                    <Plus className='w-5 h-5 group-hover:scale-105 transition'/>
+                }
+            </button>
+        </div>
+
+    </div>
+  )
+}
+
+export default UserCard
--- Client/src/pages/Discover.tsx
+++ Client/src/pages/Discover.tsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import { useState } from 'react'
+import { type User } from '../assets/assets'
+import { dummyConnectionsData } from '../assets/assets'
+import { Search } from 'lucide-react'
+import UserCard from '../components/UserCard'
+import Loading from '../components/Loading'
+
 
 const Discover = () => {
+
+  const [input , setInput] = useState<string>('')
+  const [users, setUsers] = useState<User[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter'){
+      setUsers([])
+      setLoading(true)
+      setTimeout(() => {
+        setUsers(dummyConnectionsData)
+        setLoading(false)
+      },1000)
+    }
+  }
+
+
   return (
-    <div>Discover</div>
+    <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
+      <div className='max-w-6xl mx-auto p-6'>
+        {/* Title */}
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">Discover People</h1>
+          <p className="text-slate-600">Connect with Amazing people and grow your network</p>
+        </div>
+
+        {/* Search */}
+        <div className='mb-8 shadow-md rounded-md border border-slate-200/60 bg-white/80'>
+          <div className='p-6'>
+            <div className='relative'>
+              <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-slate-400' />
+              <input type='text' value={input} onChange={(e)=> setInput(e.target.value)} onKeyUp={handleSearch} 
+              placeholder='Search by name, username, bio or location....' className='w-full pl-10 sm:pl-12 py-2 
+              border border-gray-300 rounded-md max-sm:text-sm' />
+            </div>
+          </div>
+        </div>
+
+        <div className='flex flex-wrap gap-3'>
+          {users.map((user) =>(
+            <UserCard user={user} key={user._id} />
+          ))}
+        </div>
+
+        {
+          loading && (<Loading height='60vh' />)
+        }
+      </div>
+    </div>
   )
 }
 
