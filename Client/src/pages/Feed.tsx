9f1255e5976146290e0be3d2f769c005ea4c6c32
Yashikagoyal2103 <yashikagoyal2005@gmail.com>
2025-08-22 22:23:36 +0530

    Connection page added

--- Client/src/pages/Feed.tsx
+++ Client/src/pages/Feed.tsx
@@ -4,6 +4,8 @@ import Loading from '../components/Loading';
 import { type Post } from '../assets/assets';     //type is written to separate value import from interface/type import
 import StoriesBar from '../components/StoriesBar';
 import PostCard from '../components/PostCard'
+import RecentMessages from '../components/RecentMessages';
+import {assets} from '../assets/assets';
 
 
 const Feed = () => {
@@ -36,10 +38,16 @@ const Feed = () => {
 
       {/* Right Sidebar */}
       <div>
-        <div >
-          <h1>Sponsored</h1>
-        </div>
-        <h1> Recent messages </h1>
+          <div className="max-xl:hidden sticky top-4">
+            <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
+              <h3 className="text-slate-800  font-semibold">Sponsored</h3>
+              <img className="w-75 h-50 rounded-md" alt="" src={assets.sponsored_img} />
+              <p className="text-slate-600">Email marketing</p>
+              <p className="text-slate-400">Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
+            </div>
+          </div>
+
+        <RecentMessages />
       </div>
     </div>
   ) : (
